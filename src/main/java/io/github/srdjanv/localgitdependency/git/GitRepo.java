1b3aa60d53cd3ec410b59d2781561c286a298e38
SrdjanV <SrdjanV@protonmail.com>
2023-09-19 00:39:43 +0200

    Fixed GitRepo resource closing

--- src/main/java/io/github/srdjanv/localgitdependency/git/GitRepo.java
+++ src/main/java/io/github/srdjanv/localgitdependency/git/GitRepo.java
@@ -1,9 +1,10 @@
 package io.github.srdjanv.localgitdependency.git;
 
 import io.github.srdjanv.localgitdependency.util.FileUtil;
-import java.util.Collections;
+import java.io.IOException;
 import java.util.List;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.Constants;
 
@@ -15,52 +16,56 @@ public class GitRepo implements AutoCloseable {
     private final String headID;
     private final List<String> localChanges;
 
-    public GitRepo(GitInfo gitInfo) throws Exception {
+    public GitRepo(GitInfo gitInfo) throws GitAPIException, IOException, GitRepoInitializationException {
         this.gitInfo = gitInfo;
         {
-            Git git;
-            boolean cloned;
+            boolean cloned = false;
+            Git git = null;
             try {
                 git = Git.open(FileUtil.concat(gitInfo.getDir(), Constants.DOT_GIT));
-                cloned = false;
-
             } catch (RepositoryNotFoundException initRepo) {
-                git = GitUtils.cloneRepo(gitInfo);
                 cloned = true;
+                git = GitUtils.cloneRepo(gitInfo);
+            } finally {
+                this.git = git;
+                this.cloned = cloned;
             }
-
-            this.git = git;
-            this.cloned = cloned;
         }
 
-        this.remoteBranchName = GitUtils.getCorrespondingRemoteBranchNameFromCurrentTarget(this);
-        if (cloned) {
-            GitUtils.updateConfigAndFetch(this);
-            GitUtils.checkout(this);
-        } else {
-            final String remoteUrl =
-                    git.getRepository().getConfig().getString("remote", Constants.DEFAULT_REMOTE_NAME, "url");
-
-            if (remoteUrl == null) {
-                throw new Exception(String.format(
-                        "The repo has no remote url, delete directory: %s and try again", gitInfo.getDir()));
-            } else if (!remoteUrl.equals(gitInfo.getUrl()))
-                throw new Exception(String.format(
-                        "The repo has a different remote url, delete directory: %s and try again", gitInfo.getDir()));
-
-            if (!gitInfo.getTarget()
-                    .equals(gitInfo.getDependency().getPersistentInfo().getGitTarget())) {
+        try {
+            this.remoteBranchName = GitUtils.getCorrespondingRemoteBranchNameFromCurrentTarget(this);
+            if (cloned) {
                 GitUtils.updateConfigAndFetch(this);
-                gitInfo.getDependency().getPersistentInfo().setGitTarget(gitInfo.getTarget());
-            }
-        }
+                GitUtils.checkout(this);
+            } else {
+                final String remoteUrl =
+                        git.getRepository().getConfig().getString("remote", Constants.DEFAULT_REMOTE_NAME, "url");
 
-        this.headID = GitUtils.getRepoHead(this);
-        this.localChanges = GitUtils.getLocalChanges(this);
+                if (remoteUrl == null) {
+                    throw new Exception(String.format(
+                            "The repo has no remote url, delete directory: %s and try again", gitInfo.getDir()));
+                } else if (!remoteUrl.equals(gitInfo.getUrl()))
+                    throw new Exception(String.format(
+                            "The repo has a different remote url, delete directory: %s and try again",
+                            gitInfo.getDir()));
+
+                if (!gitInfo.getTarget()
+                        .equals(gitInfo.getDependency().getPersistentInfo().getGitTarget())) {
+                    GitUtils.updateConfigAndFetch(this);
+                    gitInfo.getDependency().getPersistentInfo().setGitTarget(gitInfo.getTarget());
+                }
+            }
+
+            this.headID = GitUtils.getRepoHead(this);
+            this.localChanges = GitUtils.getLocalChanges(this);
+        } catch (Exception e) {
+            close();
+            throw new GitRepoInitializationException(e);
+        }
     }
 
     public List<String> getLocalChanges() {
-        return localChanges == null ? Collections.emptyList() : localChanges;
+        return localChanges;
     }
 
     public Git getGit() {
@@ -86,5 +91,11 @@ public class GitRepo implements AutoCloseable {
     @Override
     public void close() {
         if (git != null) git.close();
+    }
+
+    public static class GitRepoInitializationException extends Exception {
+        public GitRepoInitializationException(Exception e) {
+            super(e);
+        }
     }
 }
